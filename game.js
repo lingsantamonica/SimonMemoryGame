878c4481d908fdfe242bd9b4d9157e7302355376
lingsantamonica <57182395+lingsantamonica@users.noreply.github.com>
2020-11-18 13:56:43 +0800

    Add initial simon game files

---
+++ game.js
@@ -0,0 +1,104 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var started = false;
+var level = 0;
+
+$(".btn").click(function() {
+
+  var userChosenColour = this.id;
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
+
+});
+
+$(document).keydown(function(event) {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+
+  }
+
+
+});
+
+function startOver()
+{
+  level=0;
+  gamePattern=[];
+  userClickedPattern=[];
+  started = false;
+}
+
+function checkAnswer(currentLevel) {
+
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    console.log("success");
+
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
+  }
+   else
+    {
+
+    console.log("wrong");
+    playSound("wrong");
+    $("#level-title").text(" Game Over, Press Any Key to Restart");
+
+    $(document.body).addClass("game-over");
+    setTimeout(function() {
+      $(document.body).removeClass("game-over");
+      }, 200);
+
+      startOver();
+  }
+
+}
+
+
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+
+  setTimeout(function() {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+
+}
+
+
+
+function playSound(name) {
+
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+
+
+
+function nextSequence() {
+
+  userClickedPattern = [];
+
+  level++;
+
+  $("#level-title").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+
+  var randomChosenColour = buttonColours[randomNumber];
+
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColour);
+
+}
